8f2689c068265759bda24b281e7dd05301e41f31
Felipe Souza <fnasci@gmail.com>
2020-08-17 19:40:08 -0300

    updating database structure

--- database.sql
+++ database.sql
@@ -1,9 +1,12 @@
+DROP DATABASE IF EXISTS launchstoredb;
+CREATE DATABASE launchstoredb;
+
 CREATE TABLE "products" (
   "id" SERIAL PRIMARY KEY,
   "category_id" integer NOT NULL,
   "user_id" integer,
   "name" text NOT NULL,
-  "description" texto NOT NULL,
+  "description" text NOT NULL,
   "old_price" integer,
   "price" integer NOT NULL,
   "quantity" integer DEFAULT 0,
@@ -27,3 +30,39 @@ CREATE TABLE "files" (
 ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 
 ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "cpf_cnpj" integer UNIQUE NOT NULL,
+  "cep" text,
+  "address" text,
+  "created_at" timestamp DEFAULT 'now()',
+  "updated_at" timestamp DEFAULT 'now()'
+);
+
+--FOREING KEY
+ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+-- CREATE PROCEDURE
+CREATE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- AUTO UPDATED_AT PRODUCTS
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- AUTO UPDATED_AT USERS
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
